61d8095b335adbfc5c6a6d607f96ec21dbcac199
sonja-ops <sodju@hotmail.com>
2025-03-17 19:53:33 +0100

    slettet og skrev på nytt

--- script.js
+++ script.js
@@ -31,3 +31,6 @@ const addTodo = document.querySelector("#addTodo");
 addTodo.addEventListener("click", function () {
   console.log("Knappen er trykket!");
 });
+
+const unitInputValue=unitInput.value;
+console.log(unitInputValue);
